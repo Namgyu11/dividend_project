a964e5e0e88fedda7d7582c4cbbc7dc8c7548992
Namgyu11 <hagaji3@naver.com>
2023-12-08 20:44:39 +0900

    스케쥴러 구현

--- src/main/java/com/dayone/dividend_project/DividendProjectApplication.java
+++ src/main/java/com/dayone/dividend_project/DividendProjectApplication.java
@@ -3,8 +3,10 @@ package com.dayone.dividend_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DividendProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dayone/dividend_project/persist/entity/DividendEntity.java
+++ src/main/java/com/dayone/dividend_project/persist/entity/DividendEntity.java
@@ -1,10 +1,7 @@
 package com.dayone.dividend_project.persist.entity;
 
 import com.dayone.dividend_project.model.Dividend;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,6 +14,13 @@ import java.time.LocalDateTime;
 @Setter
 @ToString
 @NoArgsConstructor
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {"companyId", "date"}
+                )
+        }
+)
 public class DividendEntity {
 
     @Id
